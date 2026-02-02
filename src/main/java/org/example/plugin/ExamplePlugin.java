b649093a60086b31b3df780c31af6667b33c1d30
Reign-in-blood <canni.gaming@gmail.com>
2026-02-02 15:52:06 +0100

    Make startup broadcast resilient

--- src/main/java/org/example/plugin/ExamplePlugin.java
+++ src/main/java/org/example/plugin/ExamplePlugin.java
@@ -6,6 +6,7 @@ import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
 import javax.annotation.Nonnull;
+import java.lang.reflect.Method;
 
 /**
  * This class serves as the entrypoint for your plugin. Use the setup method to register into game registries or add
@@ -28,6 +29,34 @@ public class ExamplePlugin extends JavaPlugin {
     protected void start() {
         String loadedMessage = "TaleMon loaded";
         LOGGER.atInfo().log(loadedMessage);
-        this.getServer().getPlayerManager().broadcast(Message.raw(loadedMessage));
+        broadcastToPlayers(loadedMessage);
+    }
+
+    private void broadcastToPlayers(String message) {
+        Message rawMessage = Message.raw(message);
+        Object target = invokeNoArg(this, "getServer");
+        if (target == null) {
+            target = this;
+        }
+        Object playerManager = invokeNoArg(target, "getPlayerManager");
+        if (playerManager == null) {
+            LOGGER.atWarning().log("Unable to broadcast TaleMon startup message (player manager unavailable).");
+            return;
+        }
+        try {
+            Method broadcast = playerManager.getClass().getMethod("broadcast", Message.class);
+            broadcast.invoke(playerManager, rawMessage);
+        } catch (ReflectiveOperationException ex) {
+            LOGGER.atWarning().log("Unable to broadcast TaleMon startup message (broadcast method unavailable).");
+        }
+    }
+
+    private Object invokeNoArg(Object target, String methodName) {
+        try {
+            Method method = target.getClass().getMethod(methodName);
+            return method.invoke(target);
+        } catch (ReflectiveOperationException ex) {
+            return null;
+        }
     }
 }
